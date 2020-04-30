f5fa27a4ae83733cb215ae004e97265d820b12ed
David Reeves <djreeves2005@gmail.com>
2020-04-30 13:42:38 -0400

    Changing to check page. Use design for spectators and student review. Will be live on spectator view and will be static on student view. Adding signature plugin?

--- RobofestApp/RobofestApp/RobofestApp/App.xaml.cs
+++ RobofestApp/RobofestApp/RobofestApp/App.xaml.cs
@@ -14,7 +14,7 @@ namespace RobofestApp
         public App()
         {
             InitializeComponent();
-            MainPage = new NavigationPage(new LoginPage());
+            MainPage = new NavigationPage(new CheckScore());
         }
 
         protected override void OnStart()
--- RobofestApp/RobofestApp/RobofestApp/Pages/FieldSelectionPage.xaml.cs
+++ RobofestApp/RobofestApp/RobofestApp/Pages/FieldSelectionPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.StyleSheets;
 using Xamarin.Forms.Xaml;
 
 namespace RobofestApp.Pages
@@ -23,6 +24,7 @@ namespace RobofestApp.Pages
         private static int[] Rounds = new int[6];
         private static bool[] Reruns = new bool[6];
         private static bool[] Tests = new bool[6];
+        private static bool[] Validates = new bool[6];
         private static int SelectedIndex = 0;
         public FieldSelectionPage()
         {
@@ -49,107 +51,140 @@ namespace RobofestApp.Pages
         private void ChangeField(object sender, EventArgs e)
         {
             var converter = new ColorTypeConverter();
+            if (Validates[0])
+            {
+                f1select.Style = (Style)Resources["judgePositive"] as Style;
+            }
+            else
+            {
+                f1select.Style = (Style)Resources["judgeInvalid"] as Style;
+            }
+            if (Validates[1])
+            {
+                f2select.Style = (Style)Resources["judgePositive"] as Style;
+            }
+            else
+            {
+                f2select.Style = (Style)Resources["judgeInvalid"] as Style;
+            }
+            if (Validates[2])
+            {
+                f3select.Style = (Style)Resources["judgePositive"] as Style;
+            }
+            else
+            {
+                f3select.Style = (Style)Resources["judgeInvalid"] as Style;
+            }
+            if (Validates[3])
+            {
+                f4select.Style = (Style)Resources["judgePositive"] as Style;
+            }
+            else
+            {
+                f4select.Style = (Style)Resources["judgeInvalid"] as Style;
+            }
+            if (Validates[4])
+            {
+                f5select.Style = (Style)Resources["judgePositive"] as Style;
+
+            }
+            else
+            {
+                f5select.Style = (Style)Resources["judgeInvalid"] as Style;
+            }
+            if (Validates[5])
+            {
+                f6select.Style = (Style)Resources["judgePositive"] as Style;
+            }
+            else
+            {
+                f6select.Style = (Style)Resources["judgeInvalid"] as Style;
+            }
             if (sender == f1select)
             {
                 FieldNum = 1;
                 SelectedIndex = 0;
-                f1select.BackgroundColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f2select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f3select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f4select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f5select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f6select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f1select.TextColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f2select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f3select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f4select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f5select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f6select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
+                if (Validates[0])
+                {
+                    f1select.Style = (Style)Resources["judgePositiveSelected"] as Style;
+                }
+                else
+                {
+                    f1select.Style = (Style)Resources["judgeInvalidSelected"] as Style;
+                }
             }
             else if(sender == f2select)
             {
                 FieldNum = 2;
                 SelectedIndex = 1;
-                f1select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f2select.BackgroundColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f3select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f4select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f5select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f6select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f1select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f2select.TextColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f3select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f4select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f5select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f6select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
+                if (Validates[1])
+                {
+                    f2select.Style = (Style)Resources["judgePositiveSelected"] as Style;
+                }
+                else
+                {
+                    f2select.Style = (Style)Resources["judgeInvalidSelected"] as Style;
+                }
             }
             else if(sender == f3select)
             {
                 FieldNum = 3;
                 SelectedIndex = 2;
-                f1select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f3select.BackgroundColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f2select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f4select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f5select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f6select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f1select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f3select.TextColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f2select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f4select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f5select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f6select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
+                if (Validates[2])
+                {
+                    f3select.Style = (Style)Resources["judgePositiveSelected"] as Style;
+                }
+                else
+                {
+                    f3select.Style = (Style)Resources["judgeInvalidSelected"] as Style;
+                }
             }
             else if (sender == f4select)
             {
                 FieldNum = 4;
                 SelectedIndex = 3;
-                f1select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f4select.BackgroundColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f3select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f2select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f5select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f6select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f1select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f4select.TextColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f3select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f2select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f5select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f6select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
+                if (Validates[3])
+                {
+                    f4select.Style = (Style)Resources["judgePositiveSelected"] as Style;
+                }
+                else
+                {
+                    f4select.Style = (Style)Resources["judgeInvalidSelected"] as Style;
+                }
             }
             else if (sender == f5select)
             {
                 FieldNum = 5;
                 SelectedIndex = 4;
-                f1select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f5select.BackgroundColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f3select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f4select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f2select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f6select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f1select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f5select.TextColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f3select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f4select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f2select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f6select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
+                if (Validates[4])
+                {
+                    f5select.Style = (Style)Resources["judgePositiveSelected"] as Style;
+                }
+                else
+                {
+                    f5select.Style = (Style)Resources["judgeInvalidSelected"] as Style;
+                }
             }
             else if (sender == f6select)
             {
                 FieldNum = 6;
                 SelectedIndex = 5;
-                f1select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f6select.BackgroundColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f3select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f4select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f5select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f2select.BackgroundColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f1select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f6select.TextColor = (Color)converter.ConvertFromInvariantString("#ffffff");
-                f3select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f4select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f5select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
-                f2select.TextColor = (Color)converter.ConvertFromInvariantString("Color.DodgerBlue");
+                if (Validates[5])
+                {
+                    f6select.Style = (Style)Resources["judgePositiveSelected"] as Style;
+                }
+                else
+                {
+                    f6select.Style = (Style)Resources["judgeInvalidSelected"] as Style;
+                }
+            }
+            if (!Validates[SelectedIndex])
+            {
+                WarningNotValid.IsVisible = true;
+            }
+            else
+            {
+                WarningNotValid.IsVisible = false;
             }
             AssignCurrentDetails();
         }
@@ -205,7 +240,7 @@ namespace RobofestApp.Pages
                 hubConnection = new HubConnectionBuilder().WithUrl($"http://robofest.daviadoprojects.codes/scoreHub").Build();
             }
 
-            hubConnection.On<int[], string[], int[], bool[], bool[]>("fieldDefaults", (teamids, teamnumbers, rounds, reruns, tests) =>
+            hubConnection.On<int[], string[], int[], bool[], bool[], bool[]>("fieldDefaults", (teamids, teamnumbers, rounds, reruns, tests, validate) =>
             {
                 int index = 0;
                 foreach(var item in teamids)
@@ -237,7 +272,14 @@ namespace RobofestApp.Pages
                     Tests[index] = item;
                     index++;
                 }
+                index = 0;
+                foreach (var item in validate)
+                {
+                    Validates[index] = item;
+                    index++;
+                }
                 Device.BeginInvokeOnMainThread(() => {
+                    var converter = new ColorTypeConverter();
                     f1select.Text = "Field 1 (" + TeamNumbers[0] + ")";
                     f2select.Text = "Field 2 (" + TeamNumbers[1] + ")";
                     f3select.Text = "Field 3 (" + TeamNumbers[2] + ")";
@@ -250,6 +292,56 @@ namespace RobofestApp.Pages
                     f4select.IsEnabled = true;
                     f5select.IsEnabled = true;
                     f6select.IsEnabled = true;
+                    if (Validates[0])
+                    {
+                        f1select.Style = (Style)Resources["judgePositive"] as Style;
+                    }
+                    else
+                    {
+                        f1select.Style = (Style)Resources["judgeInvalid"] as Style;
+                    }
+                    if (Validates[1])
+                    {
+                        f2select.Style = (Style)Resources["judgePositive"] as Style;
+                    }
+                    else
+                    {
+                        f2select.Style = (Style)Resources["judgeInvalid"] as Style;
+                    }
+                    if (Validates[2])
+                    {
+                        f3select.Style = (Style)Resources["judgePositive"] as Style;
+                    }
+                    else
+                    {
+                        f3select.Style = (Style)Resources["judgeInvalid"] as Style;
+                    }
+                    if (Validates[3])
+                    {
+                        f4select.Style = (Style)Resources["judgePositive"] as Style;
+                    }
+                    else
+                    {
+                        f4select.Style = (Style)Resources["judgeInvalid"] as Style;
+                    }
+                    if (Validates[4])
+                    {
+                        f5select.Style = (Style)Resources["judgePositive"] as Style;
+
+                    }
+                    else
+                    {
+                        f5select.Style = (Style)Resources["judgeInvalid"] as Style;
+                    }
+                    if (Validates[5])
+                    {
+                        f6select.Style = (Style)Resources["judgePositive"] as Style;
+                    }
+                    else
+                    {
+                        f6select.Style = (Style)Resources["judgeInvalid"] as Style;
+                    }
+
                 });
                 
             });
