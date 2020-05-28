7adbb71ef00bec660fe576f7ace80ec886aade9e
David <djreeves2005@gmail.com>
2020-05-28 13:16:01 -0400

    A couple more design tests
    Start working on competition management page

---
+++ RobofestApp/RobofestApp/RobofestApp/Pages/JudgePages/CompetitionManagementPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace RobofestApp.Pages.JudgePages
+{
+    public partial class CompetitionManagementPage : ContentPage
+    {
+        public CompetitionManagementPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
